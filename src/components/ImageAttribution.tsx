5c54039a2502da89552d957335531ffa92895505
leah <theloadingpage@gmail.com>
2022-07-21 12:46:42 +0100

    image attribution destructuring+ local tests

--- src/components/ImageAttribution.tsx
+++ src/components/ImageAttribution.tsx
@@ -1,6 +1,12 @@
-import { Place } from "./Place";
+// import { Place } from "./Place";  (probably not necessary now I've used destructuring syntax, so i commented to avoid typescript warning)
 
-function ImageAttribution({photographer, imgSrc}: {photographer: string, imgSrc: string}): JSX.Element {
+function ImageAttribution({
+  photographer,
+  imgSrc,
+}: {
+  photographer: string;
+  imgSrc: string;
+}): JSX.Element {
   return (
     <p>
       <a href={imgSrc} target="_blank" rel="noreferrer">
